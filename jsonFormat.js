1d00003cfd416f36729a553a1f4559d4fdfdca35
WilliamStott <wdstott@hotmail.co.uk>
2016-11-12 21:49:55 +0000

    Add files via upload
    JsonFormat v1

---
+++ jsonFormat.js
@@ -0,0 +1,64 @@
+Hackerthon: {
+	Game: {
+			"Hacking Area 1":[
+							{},
+							{}
+						] 
+			"Hacking Area 2":[
+							{},
+							{}
+						],
+			"Hall":[
+							{},
+							{}
+						],
+			"Workshop":[
+							{},
+							{}
+						],
+			"Toilets":[
+							{},
+							{}
+						],
+				}
+			]
+		},
+	Stats: [
+		{
+			name:"Hunger",
+			val: 0
+		},
+		{
+			name:"Thirst",
+			val: 0
+		},
+		{
+			name:"Insanity",
+			val: 0
+		},
+		{
+			name:"Tiredness",
+			val: 0
+		}
+	
+	],
+	Classes: {},
+	RandomEvents: [
+	{
+		name:"Heart Attack",
+		chance: 0.01
+	},
+		{
+		name:"Code bug",
+		chance: 10
+	},
+	{
+		name:"Heart Attack",
+		chance: 0.01
+	},
+	],
+	Progress: 0
+	
+	}
+	
+	
